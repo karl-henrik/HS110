cdfa7c1f9657fb55e0d7bcb5e2def1c583ffab76
Karl-Henrik Nilsson <kanilss@microsoft.com>
2020-01-28 17:31:41 -0800

    code move

---
+++ README.md
@@ -0,0 +1,30 @@
+# tplink wifi smart plug +
+# tplink wifi smart led bulb with color (hue)
+
+### Update 28 March 17
+Thanks to [LucidObscurity](https://github.com/LucidObscurity "LucidObscurity") 
+Code for TP-Link's smart bulbs added, guess LB100, LB110, LB120, LB130. Unable to test yet because
+they currently not available in Germany. But code looks pretty good. 👍
+
+---
+### Inital. Get things switched...
+
+Code for TP-Link's WiFi smart plugs HS100 and HS110.
+LB130.
+
+TurnOn, TurnOff, DeviceInfo, etc. - see tplink-smarthome-commands.txt for more commands to integrate.
+
+![alt tag](https://raw.githubusercontent.com/iqmeta/tplink-smartplug/master/tplink-smartp.png)
+
+Just a little reference C# implementation, thanks goes for the most work on this to:
+
+- https://georgovassilis.blogspot.sg/2016/05/controlling-tp-link-hs100-wi-fi-smart.html	
+
+- https://www.softscheck.com/en/reverse-engineering-tp-link-hs110/ 	
+  (Code: https://github.com/softScheck/tplink-smartplug)
+
+- https://github.com/plasticrake/hs100-api
+
+- https://github.com/sausheong/hs1xxplug
+
+have fun.
---
+++ com.iqmeta.tplink-smartplug/Program.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace com.iqmeta.tplink_smartplug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string bulbIP = "192.168.1.2";
+            string plugOrSwitchIP = "192.168.1.243";
+            try
+            {
+                //var state = new BulbState();
+                //state.transition_light_state.on_off = 1;
+                //state.transition_light_state.brightness = 50;
+                //dynamic bulbResponse = Utils.SendToSmartBulb(bulbIP, state);
+                
+                //Console.Write(JsonConvert.SerializeObject(bulbResponse, Formatting.Indented));
+                //Console.ReadKey();
+
+                dynamic plugResponse = Utils.SendToSmartPlugOrSwitch(plugOrSwitchIP, Commands.SysInfoAndEmeter());
+                Console.WriteLine(JsonConvert.SerializeObject(plugResponse, Formatting.Indented));
+                Console.ReadKey();
+
+                //Utils.SendToSmartPlugOrSwitch(plugOrSwitchIP, Commands.TurnOn());
+                //Console.ReadKey();
+
+                //Utils.SendToSmartPlugOrSwitch(plugOrSwitchIP, Commands.TurnOff());
+
+                //dynamic stats = Utils.SendToSmartPlugOrSwitch(plugOrSwitchIP, Commands.MonthStats(2016));
+                //Console.Write(JsonConvert.SerializeObject(stats, Formatting.Indented));
+
+                //Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
